cb1800fcf4c15a0077ddef49f26bc070200b656a
Diego Maldonado <dj_maldonado19@hotmail.es>
2025-09-18 23:59:28 -0600

    Adding base project

---
+++ src/main/java/com/ayd/sie/SieApplication.java
@@ -0,0 +1,13 @@
+package com.ayd.sie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SieApplication.class, args);
+	}
+
+}
